1df4274763e890c253d5eb1eac6fd56e6b4974e3
greisyvirgen <gvirgen712@gmail.com>
2022-12-02 11:50:53 -0500

    Added files

---
+++ Day1 Challenge.py
@@ -0,0 +1,30 @@
+# Greisy Virgen Larios
+
+# In case the Elves get hungry and need extra snacks, they need to know which Elf
+# to ask: they'd like to know how many Calories are being carried by the Elf carrying
+# the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+# Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+def calorie_count():
+    file = open('day1.txt', 'r')
+    line = file.readlines()
+
+    stats = {}
+    elf_num = 1
+    calories = 0
+
+    for cal in line:
+        if cal.strip():
+            # If the line has a value,
+            # we add it to count
+            calories += int(cal)
+
+        else:
+            # We reached a blank line, add total calories
+            # carried by current elf and move to the next.
+            stats[f"Elf {elf_num}"] = calories
+            elf_num += 1
+            calories = 0
+
+    file.close()
+    return max(stats.values())
